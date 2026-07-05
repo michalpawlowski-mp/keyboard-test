51417a68e8d7a74f70fade1218cb074cd25bfe16
Michal <kontakt@michalpawlowski.pl>
2026-07-05 22:42:18 +0200

    feat: add keyboard and mouse layout data

---
+++ src/mouseKeys.js
@@ -0,0 +1,9 @@
+const mouseKeys = [
+  { code: 0, label: "Lewy przycisk" },
+  { code: 1, label: "Prawy przycisk" },
+  { code: 2, label: "Scroll" },
+  { code: 3, label: "Wstecz" },
+  { code: 4, label: "Dalej" },
+];
+
+export default mouseKeys;
